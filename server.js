7191dd8cd65baeb754bddfa38b69bae2c8a6b004
azotamiota <azotamiota@gmail.com>
2022-07-14 14:03:54 +0100

    Task done

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 
+    // console.log(req.url);
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.end('Welcome')
+    } else if (req.url === '/cats') {
+        res.statusCode = 200;
+        res.end('This is a cat')
+    } else {
+        res.statusCode = 404;
+        res.end('404 not found')
+    }
+
+    res.statusCode = 200;
+    res.end('Hi')
 })
 
-server.listen(80, () => {
-    console.log('Server listening at port 80')
+server.listen(3000, () => {
+    console.log('Server listening at port 3000')
 });
